bcfc8643eb06269362107677d5761480c242dd8c
Nebaki <Kiya8199@gmail.com>
2022-11-07 00:24:33 -0300

    ui

--- lib/screen/Home/hompage.dart
+++ lib/screen/Home/hompage.dart
@@ -2,13 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
+import '../drawer/drawer.dart';
+
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Home page"),
+    return  Scaffold(
+     appBar: AppBar(),
+     drawer: drawer(),
+      body: SafeArea(child: Center(child: Text("Homepage"),)),
     );
   }
 }
